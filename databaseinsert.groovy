26d91802807a58e89b7d1a11ee94fee1521ceecf
Tien <tien-nguyen1@hotmail.co.uk>
2018-05-07 18:34:55 +0100

    Update databaseinsert.groovy

--- databaseinsert.groovy
+++ databaseinsert.groovy
@@ -1,4 +1,36 @@
 package com.database
 
+import groovy.sql.Sql
+
 class databaseinsert {
+    static void main (String[] args){
+
+        def sql = null;
+        def url = 'jdbc:mysql://localhost:3306/shopcart?useLegacyDatetimeCode=false&serverTimezone=UTC';
+        def driver = 'com.mysql.cj.jdbc.Driver';
+        def user = 'root';
+        def password = '';
+
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+        try {
+             sql = Sql.newInstance(url, user, password, driver)
+        } catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+        }
+
+        def insertsql = 'INSERT INTO customer(name,email,phonenum)VALUE(?,?,?)'
+        // Example of input
+        def params = ['apps','apps@hotmail.com',12321321]
+        try {
+            def keys = sql.executeInsert(insertsql,params)
+        } catch (ClassNotFoundException e){
+            System.out.println(e.getMessage());
+
+        }
+
+    }
 }
